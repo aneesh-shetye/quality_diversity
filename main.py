f699b7ddfee83b8c0f0cec4743a4507c1fa64245
Aneesh Shetye <axs10302@log-2.hpc.nyu.edu>
2025-04-19 14:29:30 -0400

    mod agent

--- main.py
+++ main.py
@@ -88,42 +88,82 @@ class Sampler:
 
 #initial_template: str 
 initial_code_hopper = """
-#Build an agent to solve an environment.  
-#The  environment has a hopper - a two-dimensional one-legged figure consisting of four main body parts - the torso at the top, the thigh in the middle, the leg at the bottom, and a single foot on which the entire body rests. The goal is to make hops that move in the forward (right) direction by applying torque to the three hinges that connect the four body parts. 
+Build an agent to solve an environment.  
+edit the previous agent `agent_v0`. Name the new agent as `agent_v1` and define it within the `Agent` class. 
+The  environment has a hopper - a two-dimensional one-legged figure consisting of four main body parts - the torso at the top, the thigh in the middle, the leg at the bottom, and a single foot on which the entire body rests. The goal is to make hops that move in the forward (right) direction by applying torque to the three hinges that connect the four body parts. 
 
-#The input to this agent  is the current state of the environment. Its output should be an action of the form (float, float, float) where each value ranges from -1 to 1.
+The input to this agent  is the current state of the environment. Its output should be an action of the form (float, float, float) where each value ranges from -1 to 1.
 
-#This output would represent torques applied on rotors such that: 
-#action[0] = torque applied on the thigh rotor
-#action[1] = torque applied on the leg rotor 
-#action[2] = torque applied on the foot rotor
+This output would represent torques applied on rotors such that: 
+action[0] = torque applied on the thigh rotor
+action[1] = torque applied on the leg rotor 
+action[2] = torque applied on the foot rotor
 
-#The function you are designing would take in a "state" argument which is a 11 dimensional vector:
-#state[0] gives the z-coordinate of the torso (height of the hopper),
-#state[1] gives the angle of the torso, 
-#state[2] gives the angle of the thigh joint, 
-#state[3] gives the angle of the foot joint and, 
-#state[4] gives the velocity of the x-coordinate (height) of the torso
-#state[5] gives the velocity of the x-coordinate of the torso 
-#state[6] gives the velocity of the z-coordinate of the torso 
-#state[7] gives the angular velocity of the angle of the torso 
-#state[8] gives the angular velocity of the thigh hinge 
-#state [9] gives the angular velocity of the leg hinge 
-#state[10] gives the angular velocity of the foot hinge 
+The function you are designing would take in a "state" argument which is a 11 dimensional vector:
+state[0] gives the z-coordinate of the torso (height of the hopper),
+state[1] gives the angle of the torso, 
+state[2] gives the angle of the thigh joint, 
+state[3] gives the angle of the foot joint and, 
+state[4] gives the velocity of the x-coordinate (height) of the torso
+state[5] gives the velocity of the x-coordinate of the torso 
+state[6] gives the velocity of the z-coordinate of the torso 
+state[7] gives the angular velocity of the angle of the torso 
+state[8] gives the angular velocity of the thigh hinge 
+state [9] gives the angular velocity of the leg hinge 
+state[10] gives the angular velocity of the foot hinge 
 
-def agent_v0(state) -> tuple[float, float, float]:
-  #state[0] gives the z-coordinate of the torso (height of the hopper),
-  #state[1] gives the angle of the torso, 
-  #state[2] gives the angle of the thigh joint, 
-  #state[3] gives the angle of the foot joint and, 
-  #state[4] gives the velocity of the x-coordinate (height) of the torso
-  #state[5] gives the velocity of the x-coordinate of the torso 
-  #state[6] gives the velocity of the z-coordinate of the torso 
-  #state[7] gives the angular velocity of the angle of the torso 
-  #state[8] gives the angular velocity of the thigh hinge #state [9] gives the angular velocity of the leg hinge 
-  #state[10] gives the angular velocity of the foot hinge 
-  #Given the state output actions that would carry the object to the required position using the robotic arm.
+```python
+class Agent(): 
+    def __init__(self): 
+        pass 
+    
+    def agent_v0(state) -> tuple[float, float, float]:
+      #state[0] gives the z-coordinate of the torso (height of the hopper),
+      #state[1] gives the angle of the torso, 
+      #state[2] gives the angle of the thigh joint, 
+      #state[3] gives the angle of the foot joint and, 
+      #state[4] gives the velocity of the x-coordinate (height) of the torso
+      #state[5] gives the velocity of the x-coordinate of the torso 
+      #state[6] gives the velocity of the z-coordinate of the torso 
+      #state[7] gives the angular velocity of the angle of the torso 
+      #state[8] gives the angular velocity of the thigh hinge #state [9] gives the angular velocity of the leg hinge 
+      #state[10] gives the angular velocity of the foot hinge 
+      #Given the state output actions that would carry the object to the required position using the robotic arm.
+      return (0.0, 0.0, 0.0) 
+
+    #########################################
+    #your agent here
+
+    #########################################
+```
                 """
+prefix_prompt_hopper =""" 
+Build an agent to solve an environment.  
+edit the previous agent `agent_v0`. Name the new agent as `agent_v1` and define it within the `Agent` class. 
+The  environment has a hopper - a two-dimensional one-legged figure consisting of four main body parts - the torso at the top, the thigh in the middle, the leg at the bottom, and a single foot on which the entire body rests. The goal is to make hops that move in the forward (right) direction by applying torque to the three hinges that connect the four body parts. 
+
+The input to this agent  is the current state of the environment. Its output should be an action of the form (float, float, float) where each value ranges from -1 to 1.
+
+This output would represent torques applied on rotors such that: 
+action[0] = torque applied on the thigh rotor
+action[1] = torque applied on the leg rotor 
+action[2] = torque applied on the foot rotor
+
+The function you are designing would take in a "state" argument which is a 11 dimensional vector:
+state[0] gives the z-coordinate of the torso (height of the hopper),
+state[1] gives the angle of the torso, 
+state[2] gives the angle of the thigh joint, 
+state[3] gives the angle of the foot joint and, 
+state[4] gives the velocity of the x-coordinate (height) of the torso
+state[5] gives the velocity of the x-coordinate of the torso 
+state[6] gives the velocity of the z-coordinate of the torso 
+state[7] gives the angular velocity of the angle of the torso 
+state[8] gives the angular velocity of the thigh hinge 
+state [9] gives the angular velocity of the leg hinge 
+state[10] gives the angular velocity of the foot hinge 
+
+```python
+"""
 
 
 initial_code_cartpole = """
@@ -178,134 +218,39 @@ initial_code_dm = """
 def agent_v0(state) -> int: 
 """
 
+
 #INITIALIZE  DATABASE with 0 islands  
 global database
+initial_code = """
+class Agent(): 
+    def __init__(self): 
+        pass 
+    
+    def agent_v0(state) -> tuple[float, float, float]:
+      #state[0] gives the z-coordinate of the torso (height of the hopper),
+      #state[1] gives the angle of the torso, 
+      #state[2] gives the angle of the thigh joint, 
+      #state[3] gives the angle of the foot joint and, 
+      #state[4] gives the velocity of the x-coordinate (height) of the torso
+      #state[5] gives the velocity of the x-coordinate of the torso 
+      #state[6] gives the velocity of the z-coordinate of the torso 
+      #state[7] gives the angular velocity of the angle of the torso 
+      #state[8] gives the angular velocity of the thigh hinge #state [9] gives the angular velocity of the leg hinge 
+      #state[10] gives the angular velocity of the foot hinge 
+      #Given the state output actions that would carry the object to the required position using the robotic arm.
+      return (0.0, 0.0, 0.0) 
+"""
 database = {}
-#database is a dictionary with island_id as keys and Islands as values
-#Islands: 
-#attributes:
-    #codes: List[str] 
-    #scores:List[float] 
-    #best_performing_code:str
-    #best_score:float
-    #median_code_embedding: torch.tensor (median(embedding_function(codes)) 
-#methods: 
-    #add_code -> adds code to codes 
-    #update_best_performing_code 
-    #calculate_median_code_embedding
-
-#INITIALZE SAMPLER
-#sampler is a simple LLM (pipline.generate) 
-#sampler = Sampler()
-
-#INITIALIZE EVALUATOR 
-#evaluator: 
-#gets text that is corresponds to our function 
-#makes it into a runnable program
-#runs the function in the environment in a sandbox say 10 times  
-#returns a score  
-
-#INITIALIZE A DISTANCE FUNCTION e.g. DBSCAN 
-
-##process = psutils.Process(os.getpid())
-
-'''
-def monitor_memory(interval=0.5):
-    process = psutils.Process(os.getpid())
-    while True:
-        rss_mb = process.memory_info().rss / (1024 ** 2)
-        print(f"[Memory Monitor] RSS: {rss_mb:.2f} MB")
-        time.sleep(interval)
-
-# Start the memory monitor thread
-monitor_thread = threading.Thread(target=monitor_memory, daemon=True)
-monitor_thread.start()
-'''
-
-
-def process_pair(score, code, behaviour): 
-    if score is None: 
-        return 
-
-    #with lock: 
-    if len(database.keys())==0: 
-        database[0] = Island(code=[code], score=[score], behaviour=[behaviour])
-        wandb.log({f'score_island_{len(database.keys())}': score})
-        wandb.log({f'program_{len(database.keys())}':wandb.Html(f'score: {score}<pre>{code}</pre>')})
-    else: 
-        update_database(code, score, behaviour, threshold=1.5)
-
-
-def update_database(candidate_code, candidate_score, candidate_behaviour, threshold):
-    """
-    Update the global dataset with a candidate agent.
-    
-    - candidate_behaviour: A dictionary representing the probability distribution over actions.
-    - threshold: A KL divergence threshold used to decide if the candidate is too dissimilar.
-    
-    If the candidate's behaviour (as measured by KL divergence) is not similar enough to any
-    existing island, a new Island is created. Otherwise, if the candidate is similar and its score
-    is higher than the best in that island, the candidate is appended to that island and the best values are updated.
-    """
-    #print(candidate_behaviour)
-    with database_lock:
-        min_kl = float('inf')
-        closest_index = None
-        # Loop over islands and compute KL divergence.
-        for idx, island in database.items():
-            #print(f'candidate_behaviour: {candidate_behaviour}') 
-            #print(f'island.best_behaviour: {island.best_behaviour}') 
-            #divergence = kl_divergence(candidate_behaviour, island.best_behaviour)
-            divergence = wasserstein_with_weights(np.array([np.array(t) for t in candidate_behaviour.keys()]), np.array([np.array(t) for t in island.best_behaviour.keys()]), np.array([np.array(t) for t in candidate_behaviour.values()]), np.array([np.array(t) for t in island.best_behaviour.values()]))
-            if divergence < min_kl:
-                min_kl = divergence
-                closest_index = idx
-
-        # If no island is similar enough (KL divergence above threshold) or no island exists:
-        if min_kl > threshold or closest_index is None:
-            new_index = len(database)
-            database[new_index] = Island(candidate_code, candidate_score, candidate_behaviour)
-            wandb.log({f'score_island_{new_index}': candidate_score})
-            wandb.log({f'program_{new_index}':wandb.Html(f'score: {candidate_score}<pre>{candidate_code}</pre>')})
-            #print(f"Created new Island {new_index} (score: {candidate_score}, KL divergence: {min_kl:.4f})")
-        else:
-            # Candidate is similar to an existing island.
-            island = database[closest_index]
-            if candidate_score > island.best_score:
-                # Append the new candidate and update best values.
-                island.codes.append(candidate_code)
-                island.scores.append(candidate_score)
-                island.best_score = candidate_score
-                island.best_code = candidate_code
-                wandb.log({f'score_island_{closest_index}': candidate_score})
-                wandb.log({f'program_{closest_index}':wandb.Html(f'score: {candidate_score}<pre>{candidate_code}</pre>')})
-                #print(f"Updated Island {closest_index} with a higher score: {candidate_score} (prev: {island.best_score})")
-            #else:
-                #print(f"Candidate rejected for Island {closest_index} (score: {candidate_score} <= best: {island.best_score})")
-
-
+for i in range(10): 
+    database[i] = initial_code
 
 time_step = 0 
 if __name__ == "__main__": 
 
-    '''
-    if mp.get_start_method(allow_none=True) is None:
-        mp.set_start_method('spawn') 
-    '''
-
     for t in range(int(1e4)): 
-        #print("Entered the for loop")
-        #code = sampler.generate(get_prompt_from_existing_islands) 
-        #print(database) 
-        #print(f'database.keys: {database.keys()}')
 
-        if len(database.keys()) < 10: 
-            prompt = initial_code_hopper
-            prompts = [initial_code_hopper]*10
-        else: 
-            prompt = database[random.sample(list(database.keys()), 1)[0]].get_prompt()
-            prompts = [i.get_prompt() for i in database.values()]
 
+        prompts = [prefix_prompt_hopper + database[i] + '\n```'  for i in range(10)] 
         start_time = time.time()
         #print('prompting LLM') 
         print(f'prompts: {prompts}') 
@@ -313,59 +258,20 @@ if __name__ == "__main__":
         response = pipeline(prompts, max_new_tokens=1024)
         codes = [t[0]['generated_text'] for t in response]
 
-        #codes = tokenizer.batch_decode(code)  
-        #codes = code
-        #print(f'sample in sampler: {codes}')
-        #print(f"time taken to generate code: {time.time() - start_time}")
-
         start_time = time.time()
-        '''
-        with ProcessPoolExecutor(max_workers=2) as executor:
-            results = list(executor.map(evaluate_agent, codes))
-        '''
-        for c in codes: 
+
+        for i, c in enumerate(codes): 
             start_time = time.time()
             result = evaluate_agent(c)
-            #print(f"time taken to evaluate code: {time.time() - start_time}") 
-            end_time = time.time()
-            process_pair(result[1], result[0], result[2])
-            #print(f"time taken to put evaluated code into islands:{time.time() - end_time}")
+            #result: [code, total_reward, normalized_distribution, quantized action]
 
-        '''
-
-        end_time = time.time()
-
-        print(f"time taken to evaluate code: {end_time - start_time}") 
-        
-        #print(results)
-
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            # Submitting tasks for each (score, code) pair.
-            futures = [executor.submit(process_pair, result[1], result[0], result[2]) for result in results]
-            # Optionally, wait for all futures to complete.
-            for future in futures:
-                future.result()
-
-        print(f"time taken to put evaluated code into islands:{time.time() - end_time}")
-        '''
-        start_time = time.time()
-
-        #delete some islands 
-        if len(database.keys()) > 10: 
-            new_database_indices = heapq.nlargest(10, database, key=lambda database_key: database[database_key].best_score)
-            new_database = { k: database[new_database_indices[k]] for k in range(len(new_database_indices))} 
-            database = new_database
+            if result[1] not None: 
+                database[i] += c 
+                wandb.log({f'score_island_{i}': result[1]})
+                wandb.log({f'program_{i}':wandb.Html(f'<pre>{c}</pre>')})
 
 
         '''
-        results =  Parallel(n_jobs=-1, backend="multiprocessing")(delayed(wasserstein_with_weights)(np.array([np.array(t) for t in database[i].best_behaviour.keys()]), np.array([np.array(t) for t in database[j].best_behaviour.keys()]), np.array([np.array(t) for t in database[i].best_behaviour.values()]), np.array([np.array(t) for t in database[j].best_behaviour.values()]))
-                                                                  for i in database.keys()
-                                                                  for j in database.keys())
-        '''
-
-        #wasserstein_matrix = np.array(results).reshape(len(database.keys()), -1)
-
-        #print(f"time taken to draw the wasserstein matrix: {time.time() - start_time}")
         wasserstein_matrix = np.zeros((len(database.keys()), len(database.keys())))
 
         for i in database.keys(): 
@@ -389,6 +295,7 @@ if __name__ == "__main__":
         plt.title("Pairwise Wasserstein Distance")
 
         wandb.log({"wasserstein_distance_between_island_(best)_behaviours":wandb.Image(plt)})
+        '''
 
 
 
